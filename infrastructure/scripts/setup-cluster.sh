77014d64b4ccbe805bc3fed7a4d869db19d42b0b
AkiraTomori <ThaiMinhHuy1407@gmail.com>
2026-05-24 13:27:31 +0000

    feat: add Stakater chart repository to setup-cluster script

--- infrastructure/scripts/setup-cluster.sh
+++ infrastructure/scripts/setup-cluster.sh
@@ -10,6 +10,7 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo add jetstack https://charts.jetstack.io
+helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo update
 
 #Read configuration value from cluster-config.yaml file
